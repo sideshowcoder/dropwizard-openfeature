108aa60647ee9ac83ffde3e7df32cbddc602f2b8
Philipp Fehre <philipp@fehre.co.uk>
2025-05-09 13:59:41 +0200

    TODOs

--- src/main/java/io/github/sideshowcoder/dropwizard_openfeature/OpenFeatureBundle.java
+++ src/main/java/io/github/sideshowcoder/dropwizard_openfeature/OpenFeatureBundle.java
@@ -3,6 +3,16 @@ package io.github.sideshowcoder.dropwizard_openfeature;
 import io.dropwizard.core.ConfiguredBundle;
 import io.dropwizard.core.setup.Environment;
 
+/**
+ * TODO tests for OpenFeatureBundle integration into a Dropwizard Application
+ * TODO tests for configured flagd provider with flags serverd from offlineflagspath
+ * TODO tests for configured flagd provider with flags read from file via running flagd in testcontainer
+ * TODO tests for InMemoryProvider serving flags
+ * TODO github workflow to publish to github maven
+ * TODO publish to maven-central
+ * TODO get listed in dropwizard modules
+ * TODO get listed in openfeature.dev
+ */
 public class OpenFeatureBundle implements ConfiguredBundle<OpenFeatureBundleConfiguration> {
     @Override
     public void run(OpenFeatureBundleConfiguration configuration, Environment environment) {
--- src/main/java/io/github/sideshowcoder/dropwizard_openfeature/OpenFeatureBundleConfiguration.java
+++ src/main/java/io/github/sideshowcoder/dropwizard_openfeature/OpenFeatureBundleConfiguration.java
@@ -1,14 +1,5 @@
 package io.github.sideshowcoder.dropwizard_openfeature;
 
-/**
- * TODO extract Bundle out of Scratchpad
- * TODO move flagd to test container
- * TODO Add tests for OpenFeatureBundle integration test -> FlagD with offlineflags, and inmemory provider
- * TODO add Documentation
- * TODO Message OpenFeature folks
- * TODO Message Dropwizard folks
- */
-
 public interface OpenFeatureBundleConfiguration {
     OpenFeatureBundleFactory getOpenFeatureBundleFactory();
 }
