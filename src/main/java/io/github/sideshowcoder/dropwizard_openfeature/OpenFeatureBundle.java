f7861de5225efb05a5f1c6ed1b6f966e5b740cba
Philipp Fehre <philipp@fehre.co.uk>
2025-06-06 15:46:21 +0200

    Update OpenFeatureBundle.java

--- src/main/java/io/github/sideshowcoder/dropwizard_openfeature/OpenFeatureBundle.java
+++ src/main/java/io/github/sideshowcoder/dropwizard_openfeature/OpenFeatureBundle.java
@@ -10,16 +10,6 @@ import io.github.sideshowcoder.dropwizard_openfeature.health.OpenFeatureHealthCh
 
 import java.util.Map;
 
-/**
- * TODO tests for OpenFeatureBundle integration into a Dropwizard Application
- * TODO tests for configured flagd provider with flags serverd from offlineflagspath
- * TODO tests for configured flagd provider with flags read from file via running flagd in testcontainer
- * TODO tests for InMemoryProvider serving flags
- * TODO github workflow to publish to github maven
- * TODO publish to maven-central
- * TODO get listed in dropwizard modules
- * TODO get listed in openfeature.dev
- */
 public class OpenFeatureBundle implements ConfiguredBundle<OpenFeatureBundleConfiguration> {
 
     private FeatureProvider featureProvider;
